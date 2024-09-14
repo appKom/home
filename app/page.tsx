de6705111cf616d36760c7f5645c59be62ce0223
fredrir <fhansteen@gmail.com>
2024-09-14 13:41:41 +0200

    started on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,55 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Appkom</h1>
-      </main>
+    <div className="w-full flex justify-center min-h-screen">
+      <div className="py-6 px-6 w-full max-w-screen-lg text-gray-700">
+        <main className="flex flex-col">
+          <div className="flex flex-row justify-between">
+            <div>
+              <div className="py-6 flex flex-row justify-between">
+                <h1 className="text-3xl font-semibold ">
+                  Applikasjonskomiteen
+                </h1>
+                <Image
+                  src={"/logos/appkom-logo.svg"}
+                  alt={"Appkom logo"}
+                  width={200}
+                  height={200}
+                />
+              </div>
+              <article>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+                numquam fugit adipisci dicta alias explicabo saepe. Sit aperiam,
+                adipisci aut quis, ipsum voluptates sunt quisquam velit
+                distinctio voluptatem quo. Quam? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Officia ut recusandae laborum odio
+                impedit, consectetur possimus mollitia distinctio voluptates,
+                voluptas unde quidem accusamus. Maiores error neque quia labore
+                ipsa ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
+                elit. Provident, distinctio debitis. At magni quae cumque error
+                odit ab voluptatibus dolorem impedit vero adipisci, qui
+                assumenda, cum quam iusto vel voluptas. Lorem, ipsum dolor sit
+                amet consectetur adipisicing elit. Aliquam hic modi iusto
+                nesciunt illo impedit. Ad modi eaque, commodi totam eligendi
+                esse beatae temporibus, reiciendis consequatur repellendus error
+                tenetur laborum. Lorem ipsum dolor sit, amet consectetur
+                adipisicing elit. Deserunt, adipisci doloribus. Pariatur, nemo!
+                Totam atque natus vitae laboriosam ullam facere nihil illo quia,
+                enim minima ab. Dolorum amet ipsam reprehenderit. Lorem ipsum
+                dolor sit amet consectetur adipisicing elit. Quos, dolorum
+                perferendis. Rem ut asperiores similique incidunt molestiae,
+                quod saepe mollitia quisquam rerum provident accusamus,
+                excepturi doloremque dolores eligendi fugit laboriosam. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                laboriosam aspernatur sunt rerum nemo earum corrupti eveniet
+                molestiae totam, impedit dolore iure, ipsa facilis excepturi
+                similique voluptas, quam asperiores ipsam.
+              </article>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,18 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Button } from "./Button";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <div className="bg-tealBlue w-full text-white">
       <div className="w-full flex justify-center">
         <nav className="flex items-center justify-between flex-wrap px-5 py-5 bg-tealBlue w-full max-w-[1920px]">
-          <h1 className="font-semibold text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            <span className="block sm:hidden">Appkom</span>
-            <span className="hidden sm:block">Applikasjonskomiteen</span>
-          </h1>
+          <Link href={"/"}>
+            <h1 className="font-semibold text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl hover:text-gray-300">
+              <span className="block sm:hidden">Appkom</span>
+              <span className="hidden sm:block">Applikasjonskomiteen</span>
+            </h1>
+          </Link>
 
           <div className="hidden sm:block">
             <Button title="Kontakt oss" href="/kontakt" color="onlineOrange" />
