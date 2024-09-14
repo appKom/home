cdd3a92f6d2f32cff0c4df7a970150cd221b3adb
fredrir <fhansteen@gmail.com>
2024-09-14 19:06:44 +0200

    created frontend for contact us page

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [buttonText, setButtonText] = useState("Send");
+  const [, setStatus] = useState({ submitted: false, message: "" });
+
+  const handleChange = (e: { target: { name: string; value: string } }) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+
+    await fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(() => {
+        setStatus({ submitted: true, message: "Message sent successfully" });
+        setButtonText("Submit");
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
+        toast.success("Message sent successfully");
+      })
+      .catch(() => {
+        setStatus({ submitted: false, message: "Error sending message" });
+        toast.error("Error sending message");
+        setButtonText("Submit");
+      });
+  };
+
+  return (
+    <div className="w-full flex justify-center min-h-screen">
+      <div className="py-6 px-6 w-full max-w-screen-lg text-gray-700">
+        <main className="flex flex-col gap-5 pb-6">
+          <div className="w-full max-w-3xl mx-auto justify-center">
+            <p className="text-xl text-center border-solid rounded-2xl border border-2 border-gray-700 dark:border-white p-2">
+              Ønsker du å komme i kontakt med oss? <br /> Fyll ut skjemaet
+              under. <br />
+              Eller send oss en e-post på{" "}
+              <a
+                href="mailto:appkom@online.ntnu.no"
+                className="text-blue-500 underline hover:text-onlineOrange"
+              >
+                appkom@online.ntnu.no
+              </a>
+            </p>
+          </div>
+
+          <div
+            id="contact-me"
+            className="flex flex-col w-full max-w-3xl  p-4 mx-auto border-solid rounded-2xl border border-2 border-gray-700 dark:border-white"
+          >
+            <h1 className="text-center font-bold text-4xl">Kontakt oss</h1>
+            <form onSubmit={handleSubmit} className="w-full">
+              <label
+                className="text-shadow block text-xl font-medium"
+                htmlFor="name"
+              >
+                Navn:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                className="my-2 p-2 w-full rounded-3xl  border-solid border-2 border-gray-900 dark:border-white transition-none outline-none"
+              />
+              <label
+                className="text-shadow block text-xl font-medium"
+                htmlFor="email"
+              >
+                E-Post:
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="my-2 p-2 w-full rounded-3xl  border-solid border-2 border-gray-900 dark:border-white transition-none outline-none"
+              />
+              <label
+                className="text-shadow block text-xl font-medium"
+                htmlFor="phone"
+              >
+                Telefon:
+              </label>
+              <input
+                type="phone"
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="my-2 p-2 w-full rounded-3xl  border-solid border-2 border-gray-900 dark:border-white transition-none outline-none"
+              />
+              <label
+                className="text-shadow block text-xl font-medium"
+                htmlFor="message"
+              >
+                Melding:
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+                className="my-2 p-2 h-60 w-full rounded-3xl   border-solid border-2 border-gray-900 dark:border-white transition-none outline-none"
+              />
+              <div className="py-5 text-center">
+                <button
+                  type="submit"
+                  className={
+                    "px-4 py-2 rounded-md bg-onlineOrange hover:bg-orange-400 text-2xl "
+                  }
+                >
+                  {buttonText}
+                </button>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
