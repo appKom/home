3708ae6b7ec5a800d6495ac4709c05ee82ad8dcd
fredrir <fhansteen@gmail.com>
2024-09-14 13:54:09 +0200

    added more info

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,11 +6,10 @@ export default function Navbar() {
   return (
     <div className="bg-tealBlue w-full text-white">
       <div className="w-full flex justify-center">
-        <nav className="flex items-center justify-between flex-wrap px-5 py-5 bg-tealBlue w-full max-w-[1920px]">
+        <nav className="flex items-center justify-between flex-wrap px-5 py-5 bg-tealBlue w-full max-w-screen-2xl">
           <Link href={"/"}>
             <h1 className="font-semibold text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl hover:text-gray-300">
-              <span className="block sm:hidden">Appkom</span>
-              <span className="hidden sm:block">Applikasjonskomiteen</span>
+              <span>Appkom</span>
             </h1>
           </Link>
 
