77bdf42da94dd422868c61680e22f028457e229b
fredrir <fhansteen@gmail.com>
2024-09-17 21:45:40 +0200

    tweak

--- app/medlem/[id]/page.tsx
+++ app/medlem/[id]/page.tsx
@@ -142,7 +142,7 @@ export default async function MemberPage({ params }: Params) {
                 Kontakt
               </h2>
 
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-xs xs:text-xs sm:text-sm md:text:md lg:text-lg ">
                 {member.phone && (
                   <div className="flex flex-row gap-2 hover:text-onlineOrange">
                     <FiPhoneIncoming size={24} />
