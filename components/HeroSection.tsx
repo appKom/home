16faa0bb406c7051fbf180411e407eb0741fb750
Julian Ammouche Ottosen <julian.ammouche@gmail.com>
2024-11-16 13:41:49 +0100

    animated hero section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,12 +1,28 @@
 "use client"
 
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import { useEffect, useState } from "react"
+
+const words = ["fremtidens", "morgendagens", "etterlengtede", "morsomme", "kreative"]
+const shuffledWords = words.sort(() => Math.random() - 0.5);
+
+const INTERVAL_SECONDS = 3;
 
 export const HeroSection = () => {
+  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length)
+    }, 1000 * INTERVAL_SECONDS)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className="max-w-3xl mx-auto text-center min-h-screen flex flex-col justify-center">
       <motion.h1
-        className="text-4xl md:text-6xl font-bold mb-6"
+        className="text-4xl md:text-6xl font-bold mb-12"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
@@ -15,12 +31,26 @@ export const HeroSection = () => {
       </motion.h1>
 
       <motion.p
-        className="text-xl text-gray-400 mb-8"
+        className="text-xl text-gray-400 mb-16 tracking-wide"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
       >
-        Vi utvikler fremtidens digitale løsninger for informatikkstudenter ved NTNU.
+        Vi utvikler{" "}
+        <span className="inline-block">
+          <AnimatePresence mode="popLayout">
+            <motion.span
+              key={currentWordIndex}
+              className="inline-block font-semibold text-white"
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -20, opacity: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              {shuffledWords[currentWordIndex]}
+            </motion.span>
+          </AnimatePresence>
+        </span>{" "}digitale løsninger for informatikkstudenter ved NTNU.
       </motion.p>
 
       <motion.div
